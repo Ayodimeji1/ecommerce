7d8bf9672a9d124759b61ce1b283eccd700e9947
Ayodimeji Kadri <ayodimejia1@gmail.com>
2022-06-10 22:12:59 +0100

    added detailed product page

--- product/views.py
+++ product/views.py
@@ -6,6 +6,7 @@ from product.models import Product, Category
 
 
 def product(request, category_slug, product_slug):
+
     product = get_object_or_404(Product, category__slug=category_slug, slug=product_slug)
 
     return render(request, 'product/product.html', {'product': product})
