581d78bfebe335f7df2cb2e3aa775725238f46db
Ayodimeji Kadri <ayodimejia1@gmail.com>
2022-10-31 13:00:43 +0100

    Update settings.py

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', cast=bool)
 
-ALLOWED_HOSTS = config('ALLOWED_HOST')
+ALLOWED_HOSTS = ['https://ecommerce-production-5f26.up.railway.app']
 
 CSRF_TRUSTED_ORIGINS = config('CSRF_TRUSTED_ORIGINS')
 # Application definition
